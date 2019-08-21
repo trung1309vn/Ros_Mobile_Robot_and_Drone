de47ab89b4bc43c7c5715e6a4c535bc6e562e3c7
Trung Nguyen <trung.ly-phuoc.nguyen@aioz.io>
2019-08-21 14:51:17 +0700

    new

--- src/subt_seed_node.cc
+++ src/subt_seed_node.cc
@@ -191,20 +191,21 @@ not available.");
   // roll (x-axis rotation)
   double sinr_cosp = +2.0 * (q.w * q.x + q.y * q.z);
   double cosr_cosp = +1.0 - 2.0 * (q.x * q.x + q.y * q.y);
-  angles.roll = atan2(sinr_cosp, cosr_cosp);
+  auto roll = atan2(sinr_cosp, cosr_cosp);
 
   // pitch (y-axis rotation)
   double sinp = +2.0 * (q.w * q.y - q.z * q.x);
+  auto pitch;
   if (fabs(sinp) >= 1)
-      angles.pitch = copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+      pitch = copysign(M_PI / 2, sinp); // use 90 degrees if out of range
   else
-      angles.pitch = asin(sinp);
+      pitch = asin(sinp);
 
   // yaw (z-axis rotation)
   double siny_cosp = +2.0 * (q.w * q.z + q.x * q.y);
   double cosy_cosp = +1.0 - 2.0 * (q.y * q.y + q.z * q.z);  
-  angles.yaw = atan2(siny_cosp, cosy_cosp);
-  ROS_INFO("angular : yaw = %f, pitch = %f, roll = %f", angles.yaw, angles.pitch, angles.roll);
+  auto yaw = atan2(siny_cosp, cosy_cosp);
+  ROS_INFO("angular : yaw = %f, pitch = %f, roll = %f", yaw, pitch, roll);
 }
 
 /////////////////////////////////////////////////
